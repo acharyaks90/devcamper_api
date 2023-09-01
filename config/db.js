722acd924b153df3a9b2fc23322cb75191b928b5
Anil Suryavanshi <thakurinfotech@gmail.com>
2023-09-01 13:32:49 +0530

    STEP 10: CORRECTION

--- config/db.js
+++ config/db.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
     const connectDB = async()=>{
         const conn = await mongoose.connect(process.env.MONGOOSE_URI, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
             useUnifiedTopology: true
         }).catch(err=>{
             console.log(err);
