b4d0dc700b5878aac51e220b75275b347d392fa4
Anil Suryavanshi <thakurinfotech@gmail.com>
2023-09-04 18:38:43 +0530

    STEP 19: DUPLICATE HANDLING

--- middleware/error.js
+++ middleware/error.js
@@ -9,6 +9,10 @@ const errorHandler = (err, req, res, next) =>{
         const message = `Boot camp not found with id of ${err.value}`;
         error = new ErrorResponse(message, 404);
     }
+    if(err.code === 11000){
+        const message = `Duplicate field value entered`;
+        error = new ErrorResponse(message, 400);
+    }
     res.status(error.statusCode || 500).json({
         success: false,
         error: error.message || 'Server Error'
