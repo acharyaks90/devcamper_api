cb2c0d4758b3d2be1d5e929f3e5c4acdb9c3be35
Anil Suryavanshi <thakurinfotech@gmail.com>
2023-09-09 17:02:31 +0530

    STEP 31 :  LIMIT AND index

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -40,12 +40,14 @@ exports.getBootcamps = asyncHandler(async (req, res, next)=>{
     }
 
      // Pagination
-
+  const page = parseInt(req.query.page, 10) || 1;
   const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
   
 
-  query = query.limit(limit);
-//  query = query.skip(startIndex).limit(limit);
+
+  query = query.skip(startIndex).limit(limit);
 
     const bootcamps = await query;
    
