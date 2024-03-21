6c03df248abf25328b5ba51edeaa1abb905db159
Anil Suryavanshi <thakurinfotech@gmail.com>
2024-03-21 06:38:59 +0530

    STEP:63 COURSE OWNERSHIP

--- models/Course.js
+++ models/Course.js
@@ -37,6 +37,11 @@ const CourseSchema = new mongoose.Schema({
         type: mongoose.Schema.ObjectId,
         ref: 'Bootcamp',
         required: true
+    },
+    URLSearchParams:{
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
     }
 
 });
