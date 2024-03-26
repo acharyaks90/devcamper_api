b460a357e9c2b01ea093f0e81f146c94f1cf8e1f
Anil Suryavanshi <thakurinfotech@gmail.com>
2024-03-26 07:27:09 +0530

    STEP:70 Update password

--- controller/auth.js
+++ controller/auth.js
@@ -53,6 +53,36 @@ exports.getMe = asyncHandler(async(req, res, next)=>{
         data: user
     })
 })
+// @desc update Login user
+// @route PUT /api/v1/auth/updatedetails
+// @access private 
+exports.updateDetails = asyncHandler(async(req, res, next)=>{
+    const fieldsToUpdate = {
+        name: req.body.name,
+        email: req.body.email
+    }
+    const user = await User.findByIdAndUpdate((req.user.id, fieldsToUpdate, {
+        new: true,
+        runValidators: true
+    }));
+    
+    res.status(200).json({
+        success: true,
+        data: user
+    })
+})
+// @desc PUT UPDATE PASSWORD
+// @route get /api/v1/auth/updatepasssword
+// @access private 
+exports.updatePassword = asyncHandler(async(req, res, next)=>{
+    const user = await User.findById((req.user.id)).select('+password');
+    if(!await user.matchPassword(req.body.currentPassword)) {
+        return next(new ErrorResponse('password is incorrect',401))
+    }
+    user.password = req.body.newPassword;
+    await user.save();
+    sendTokenResponse(user, 200, res);
+})
 // @desc reset password
 // @route PUT /api/v1/auth/resetpassword/:resetToken
 // @access private 
@@ -118,7 +148,7 @@ exports.forgetPassword = asyncHandler(async(req, res, next)=>{
 const sendTokenResponse = (user, statusCode, res) => {
     const token = user.getSignedJwtToken();
     const options = {
-        expires: new Date(Date.now()+process.env.JWT_COOKIE_EXPIRE * 24 * 60 *60 *1000),
+        expires: new Date(Date.now()+process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true
     }
     if(process.env.NODE_ENV === 'production') {
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { register, login, getMe, forgetPassword,resetPassword} = require('../controller/auth');
+const { register, login, getMe, forgetPassword,resetPassword, updateDetails, updatePassword} = require('../controller/auth');
 const router = express.Router();
  const {protect} = require('../middleware/auth')
 router.post('/register', register);
 router.post('/login',login);
 router.get('/me',protect,getMe);
+router.put('/updatedetails',protect,updateDetails);
+router.put('/updatepassword',protect,updatePassword);
 router.post('/forgetpassword',forgetPassword);
 router.put('/resetpassword/:resettoken',resetPassword);
 
